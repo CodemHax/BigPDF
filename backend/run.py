97c3870f65da76c0f1e0a6dd155a16692ecb9199
CodemHax <175680069+CodemHax@users.noreply.github.com>
2026-04-16 18:21:46 +0530

    Update port to 8006 in startup script

--- backend/run.py
+++ backend/run.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=int(os.getenv("PORT", 8000)),
+        port=int(os.getenv("PORT", 8006)),
         limit_max_connections=1000,
         limit_concurrency=None,
         timeout_keep_alive=5,
